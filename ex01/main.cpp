f3778a3be20581d77ff6eaec84c60d0866484434
Jay Karippacheril Jacob <jaykarippacheriljacob@gmail.com>
2026-02-23 20:00:06 +0100

    Rev.2.4 Made some minor changes on the main function.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jkarippa <jkarippa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 19:42:36 by jkarippa          #+#    #+#             */
-/*   Updated: 2026/02/23 19:47:35 by jkarippa         ###   ########.fr       */
+/*   Updated: 2026/02/23 19:59:16 by jkarippa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,30 @@
 #include <iostream>
 
 int main() {
-    // PhoneBook phoneBook;
+    PhoneBook phoneBook;
     std::string command;
 
     while (true) {
         std::cout << "Enter a command (ADD, SEARCH, EXIT): ";
         std::getline(std::cin, command);
 
-        if (command == "ADD") {
-            //phoneBook.addContact();
+        if (command == "ADD")
+        {
+            phoneBook.addContact();
             std::cout << "Adding a new contact..." << std::endl;
-        } else if (command == "SEARCH") {
+        } 
+        else if (command == "SEARCH")
+        {
             //phoneBook.searchContact();
             std::cout << "Searching for a contact..." << std::endl;
-        } else if (command == "EXIT") {
+        }
+        else if (command == "EXIT")
+        {
             std::cout << "Exiting the program." << std::endl;
             break;
-        } else {
-            std::cout << "Invalid command. Please try again." << std::endl;
         }
+        else
+            std::cout << "Invalid command. Please try again." << std::endl;
     }
-
     return 0;
 }
