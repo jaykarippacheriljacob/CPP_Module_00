689801840f85c6d7300e363000e223843c5c1616
Jay Karippacheril Jacob <jaykarippacheriljacob@gmail.com>
2026-02-23 19:18:40 +0100

    Rev.2.1 Revised the class contact with class members.

--- ex01/contact.hpp
+++ ex01/contact.hpp
@@ -6,22 +6,29 @@
 /*   By: jkarippa <jkarippa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 15:02:02 by jkarippa          #+#    #+#             */
-/*   Updated: 2026/02/22 15:05:23 by jkarippa         ###   ########.fr       */
+/*   Updated: 2026/02/23 19:17:54 by jkarippa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
 
+#include <iostream>
+#include <string>
 class contact
 {
+    private:
+        std::string _firstName;
+        std::string _lastName;
+        std::string _nickname;
+        std::string _phoneNumber;
+        std::string _darkestSecret;
     public:
-        contact();
-        ~contact();
+        contact(void);
+        ~contact(void);
         
         void setContact();
-        void displayContactSummary() const;
-        void displayFull() const;
+        void displayContact() const;
 };
 
 #endif
