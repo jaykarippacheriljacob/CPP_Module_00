fcceb341391d217bc8a4a6dcd0cbee3a3eeecf79
Jay Karippacheril Jacob <jaykarippacheriljacob@gmail.com>
2026-02-23 19:56:00 +0100

    Rev.2.3 Added the makefile from ex00 and revised it with the new files from the ex01. also added the main with just print statements to test the skelton of the phonebook.

---
+++ ex01/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkarippa <jkarippa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 19:42:36 by jkarippa          #+#    #+#             */
+/*   Updated: 2026/02/23 19:47:35 by jkarippa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+#include <iostream>
+
+int main() {
+    // PhoneBook phoneBook;
+    std::string command;
+
+    while (true) {
+        std::cout << "Enter a command (ADD, SEARCH, EXIT): ";
+        std::getline(std::cin, command);
+
+        if (command == "ADD") {
+            //phoneBook.addContact();
+            std::cout << "Adding a new contact..." << std::endl;
+        } else if (command == "SEARCH") {
+            //phoneBook.searchContact();
+            std::cout << "Searching for a contact..." << std::endl;
+        } else if (command == "EXIT") {
+            std::cout << "Exiting the program." << std::endl;
+            break;
+        } else {
+            std::cout << "Invalid command. Please try again." << std::endl;
+        }
+    }
+
+    return 0;
+}
